acf8081be03ea08a292d8bd21f270e0c6916802c
Aman Khan <amanjkhan21@gmail.com>
2025-10-02 12:43:32 +0530

    Update Day004_rotate_array.cpp

--- Arrays/Easy/Day004_rotate_array.cpp
+++ Arrays/Easy/Day004_rotate_array.cpp
@@ -1,8 +1,6 @@
 /*
 GFG 160 Days Challenge - Day 4: Rotate Array
 --------------------------------------------
-Problem Statement:
-------------------
 Given an array arr[]. Rotate the array to the left (counter-clockwise direction) 
 by d steps, where d is a positive integer. Do the mentioned change in the array in place.
 
